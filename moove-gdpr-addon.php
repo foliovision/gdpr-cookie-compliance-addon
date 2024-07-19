90afa148782fefe6770049ca54f1b09008162648
fvmartin <fvmartin@users.noreply.github.com>
2024-07-19 16:47:10 +0200

    Act as if the license check has passed

--- moove-gdpr-addon.php
+++ moove-gdpr-addon.php
@@ -129,3 +129,11 @@ function gdpr_cookie_compliance_addon_load_libs() {
 	include_once dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'controllers' . DIRECTORY_SEPARATOR . 'class-moove-gdpr-updater.php';
 	include_once dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'controllers' . DIRECTORY_SEPARATOR . 'class-moove-gdpr-consent-log.php';
 }
+
+// Act as if the license check has passed
+add_action(
+	'plugins_loaded',
+	function() {
+		do_action( 'gdpr_plugin_loaded' );
+	}
+);
